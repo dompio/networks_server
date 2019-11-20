c0b9aef3c1fa12cfbcdaa504b94179f201859007
dompio <34550092+dompio@users.noreply.github.com>
2019-11-20 12:07:31 +0000

    Date and time added

--- bot.py
+++ bot.py
@@ -4,17 +4,17 @@
 #
 # Group members: Dominika Piosik, Justas Samoulis, Ben Hawke
 
-import sys
-import time
+from datetime import date
+from datetime import datetime
 import socket
 
 # Variables
-botnick = 'awesomebot'
+botnick = "awesomebot"
 # allow the user to type the server ip
-server_ip = "irc.freenode.net"
+server_ip = "chat.freenode.net"
 port = 6667
 server_address = (server_ip, port)
-channel = "##test"
+channel = "##idkwhatimtyping"
 exitcode = "exit"
 
 # Create a TCP socket
@@ -27,16 +27,26 @@ ircbot.send(bytes("NICK "+ botnick +"n", "UTF-8"))
 
 # Join channel
 # Do we need the UTF-8 encoding
-ircbot.send(bytes("JOIN "+ chan +"n", "UTF-8"))
+ircbot.send(bytes("JOIN "+ channel +"n", "UTF-8"))
 
 while 1:
     message = ircbot.recv(2048).decode("UTF-8")
     print(message)
     # if message.find("PRIVMSG") != -1:  and then split???
     if message == exitcode:
-        sendmsg("Exiting...")
+        ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Exiting..." +"n", "UTF-8"))
         ircbot.send(bytes("QUIT n", "UTF-8"))
-        return
+    
+    if message.find('!day') != -1:
+        today = date.today()
+        # Full month day, year
+        this_day = today.strftime("%B %d, %Y")
+        ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Today is " + this_day +"n", "UTF-8"))
+        
+    if message.find('!time') != -1:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Current time " + current_time +"n", "UTF-8"))
 
 
 
