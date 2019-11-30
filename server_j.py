3e155e5207480780033c7b12ca34636d73a2ef96
Justas Samuolis <samuolis.just@gmail.com>
2019-11-30 12:59:05 +0000

    Some progress on server: accepts connections, receives messages

--- server_j.py
+++ server_j.py
@@ -1,36 +1,12 @@
 import socket
-
-class Channel:
-
-    def __init__(self, server: Server, name: str):
-        self.server = server
-        self.name = name
-        self.members = set()
-
-
-class Client:
-
-    def __init__(self, server: Server, socket: socket.socket):
-        self.server = server
-        self.socket = socket
-        self.channels = {}
-        self.nickname = None
-        self.prefix = None  # just nickname or nickname + host from sockets?
-        self.messagebuffer = None # buffer to send every server refresh
-
-    def message_channel(self, channel: Channel, message: str):
-        msg = ':%s %s' % (self.prefix, message)
-        for client in channel.members:
-            client.message(msg)
-
-    def message(self, message: str):
-        self.messagebuffer += message.encode()
-
+import select
 
 class Server:
 
     # example client creation: clients[conn] = Client(self, conn)
     def __init__(self):
+        self.address = '127.0.0.1'
+        self.port = 6667
         self.channels = {}  # channel name -> Channel
         self.clients = {}   # socket -> Client
         self.nicknames = {}  # nickname -> Client
@@ -56,4 +32,73 @@ class Server:
     def run(self):
         # get and handle connections
         # every n miliseconds for each client send its messagebuffer (if not empty)
-        pass
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        try:
+            s.bind((self.address, self.port))
+        except socket.error as e:
+            print('Error occured while binding %s:%s - %s' % (self.address, self.port, e))
+            # exit (sys.exit?)
+        s.listen()
+        print('Listening on port %d...' % self.port)
+
+        while True:
+            client_sockets = [x.socket for x in self.clients.values()]
+            (rread, rwrite, rexc) = select.select([s, *client_sockets], client_sockets, [], 10)
+
+            for sock in rread:
+                if sock in self.clients:
+                    print('already in clients, wants to read')
+                    self.clients[sock].recv_data()
+                else:
+                    (conn, addr) = sock.accept()
+
+                    try:
+                        self.clients[conn] = Client(self, conn)
+                        print('Accepted connection from %s:%s.' % (addr[0], addr[1]))
+                    except socket.error as e:
+                        print('Error accepting %s:%s - %s' % (addr[0], addr[1], e))
+                        try:
+                            conn.close()
+                        except Exception:
+                            pass
+
+class Channel:
+
+    def __init__(self, server: Server, name: str):
+        self.server = server
+        self.name = name
+        self.members = set()
+
+
+class Client:
+
+    def __init__(self, server: Server, socket: socket.socket):
+        self.server = server
+        self.socket = socket
+        self.channels = {}
+        self.nickname = None
+        self.prefix = None  # just nickname or nickname + host from sockets?
+        self.messagebuffer = None # buffer to send every server refresh
+
+    def message_channel(self, channel: Channel, message: str):
+        msg = ':%s %s' % (self.prefix, message)
+        for client in channel.members:
+            # dont send a message to self
+            if client is not self:
+                client.message(msg)
+
+    def message(self, message: str):
+        self.messagebuffer += message.encode()
+
+    def recv_data(self):
+        data = self.socket.recv(2 ** 10).decode()
+        print(data)
+        # parse data here
+        # perform actions based on command
+
+    
+
+
+srv = Server()
+srv.run()
