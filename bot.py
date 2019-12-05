722717bb595b1ff47da9be4d5331c87b8db44414
dompio <34550092+dompio@users.noreply.github.com>
2019-12-05 16:31:59 +0000

    responds to private messages with fun facts

--- bot.py
+++ bot.py
@@ -8,10 +8,11 @@
 from datetime import date
 from datetime import datetime
 import socket
+import random
 
 # Variables
 botnick = "ProBot"
-server_ip = '10.0.42.17'
+server_ip = 'localhost' #set to 10.0.42.17 or 'localhost' for testing
 port = 6667
 server_address = (server_ip, port)
 channel = "#test"
@@ -36,29 +37,60 @@ def join():
 # Respond to PINGs
 def ping(): 
     ircbot.send(bytes("PONG :pingisn", "UTF-8"))
+    print('PONG')
+    
+# Receive and respond to messages
+def respond():
+    while 1:
+        # Listen for messages
+        message = ircbot.recv(2048).decode("UTF-8")
+    
+        # Print message in console
+        print(message)
+    
+        # Respond to user commands
+        # if message.find("PRIVMSG") != -1:  and then split???
+        if message.find('PING :') != -1:
+            ping()
+    
+        elif message.find('!day') != -1:
+            today = date.today()
+            # Respond with month day, year
+            this_day = today.strftime("%B %d, %Y")
+            ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Today is " + this_day +"\n", "UTF-8"))
+        
+        elif message.find('!time') != -1:
+            now = datetime.now()
+            # Respond with hh:mm:ss
+            current_time = now.strftime("%H:%M:%S")
+            ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Current time " + current_time +"\n", "UTF-8"))
+        
+        elif message == exitcode:
+            ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Exiting..." +"\n", "UTF-8"))
+            ircbot.send(bytes("QUIT n", "UTF-8"))
+        
+        elif message.find('PRIVMSG Probot'):
+            #Respond to private messages with fun facts
+            # Define the fun facts
+            random_facts = {
+                "a bolt of lightning contains enough energy to toast 100,000 slices of bread.",
+                "you can hear a blue whale's heartbeat from two miles away.",
+                "nearly 30,000 rubber ducks were lost a sea in 1992 and are still being discovered today."
+                }
+            
+            # Extract user nickname
+            nick = message[message.find(':')+len(":"):message.rfind('!')]
+            
+            # Reply with a fun fact
+            ircbot.send(bytes("PRIVMSG "+ nick + " Did you know that " + random_facts[random.randint(0,2)] + "\n", "UTF-8"))
+    
 
 # Run the bot
 connect()
 register()
 join()
-while 1:
-    message = ircbot.recv(2048).decode("UTF-8")
-    print(message)
-    # if message.find("PRIVMSG") != -1:  and then split???
-    if message == exitcode:
-        ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Exiting..." +"\n", "UTF-8"))
-        ircbot.send(bytes("QUIT n", "UTF-8"))
-    
-    if message.find('!day') != -1:
-        today = date.today()
-        # Full month day, year
-        this_day = today.strftime("%B %d, %Y")
-        ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Today is " + this_day +"\n", "UTF-8"))
+respond()
         
-    if message.find('!time') != -1:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        ircbot.send(bytes("PRIVMSG "+ channel +" :"+ "Current time " + current_time +"\n", "UTF-8"))
 
 
 
